52a41d3774c43d89ec6842f360d67ef231881be4
IngneieroGeomatico <aurearagon@gmail.com>
2025-07-03 21:17:10 +0200

    se continúa con OGR

--- Vector_provider/OGR_DataProvider.py
+++ Vector_provider/OGR_DataProvider.py
@@ -13,30 +13,42 @@ class OGR_DataProvider(BaseProvider):
         super().__init__(provider_def)
 
         self.layer = None
+        self.overwriteLinksLayer = False
         self.file = provider_def['data']
         self.collection_id = provider_def.get('name', '') 
 
-        # 1. . Si se define en el YAML
+        # 1. Si se define en el YAML
         if 'layer' in provider_def:
             self.layer = provider_def['layer']
+            self.overwriteLinksLayer = False
+            fuenteDatos.leer(capa=self.layer)
+            self._fields = fuenteDatos.obtener_atributos(self.layer)
+            self.dataset = fuenteDatos
         # 2. Si no, usar la primera capa disponible
         else:
             fuenteDatos = FuenteDatosVector(self.file)
             fuenteDatos.leer(datasetCompleto=True)
             layers = fuenteDatos.obtener_capas()
-            fuenteDatos.datasource =  None
-
+            atributosPorCapa = fuenteDatos.obtener_atributos()
+            self._fields = {}
+            for nombre_capa, atributos in atributosPorCapa.items():
+                for propiedad, valores in atributos.items():
+                    if propiedad not in self._fields:
+                        self._fields[propiedad] = valores
+            
+            
             if not layers:
                 raise ProviderQueryError(f"No hay capas disponibles en {self.file}")
             self.layer = layers[0]
             self.layersArray = layers
+            self.overwriteLinksLayer = True
+            fuenteDatos.datasource =  None
+            self.dataset = None
+            
 
     def get_fields(self):
-        # Aquí se devuelve un objeto con los atributos y su tipo
-        return {
-            'field1': 'string',
-            'field2': 'string'
-        }
+        self._fields = self.dataset.obtener_atributos(self.layer)
+        return self._fields 
 
     def query(self, offset=0, limit=10, resulttype='results',
               bbox=[], datetime_=None, properties=[], sortby=[],
@@ -54,15 +66,45 @@ class OGR_DataProvider(BaseProvider):
                     self.layer = self.layersArray[int_value]
                 except (ValueError, TypeError):
                     self.layer = layerValue
+
+        self.title = self.layer
                     
         # Se carga el dataset de la capa 
-        fuenteDatos = FuenteDatosVector(self.file)
-        fuenteDatos.leer(capa=self.layer)
-        self.dataset = fuenteDatos
+        if not self.dataset:
+            fuenteDatos = FuenteDatosVector(self.file)
+            fuenteDatos.leer(capa=self.layer)
+            self.dataset = fuenteDatos
+
+        if sortby:
+            fuenteDatos.ejecutar_sql(f'select * from "{self.layer}" ORDER BY "{sortby[0]['property']}"',
+                                    self.layer,
+                                    dialect='SQLITE'            
+            )  
+
+        if offset:
+            fuenteDatos.ejecutar_sql(f'select * from "{self.layer}" OFFSET {offset}',
+                                    self.layer            
+            )
         
+        if limit:
+            fuenteDatos.ejecutar_sql(f'select * from "{self.layer}" LIMIT {limit}',
+                                    self.layer            
+            )    
 
         # Se devuelve el resultdo
         gjson = self.dataset.exportar(EPSG_Salida=4326)
+        gjson['name'] = self.layer
+        gjson['title'] = f'Capa: {self.layer}'
+
+        if self.overwriteLinksLayer:
+            gjson['links'] = [
+                {
+                    'rel': 'collection',
+                    'type': 'application/geo+json',
+                    'title': f'Capa: {self.layer} ||',
+                    'href': f'/collections/OGR_1/items?__layer__={self.layer}'
+                }
+        ]
         return gjson
     
     def get(self, identifier):
